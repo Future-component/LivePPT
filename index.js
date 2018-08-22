b5c71f67de943ca041f22ab39d9dea092e21d6ce
beth <zhangbaige@innobuddy.com>
2018-08-22 11:33:36 +0800

    初始化插件

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * LivePPT
+ * 1.0.0
+ * Copyright (c) 2018-08-22 11:32:20 Beth
+ * 直播调用PPT
+ * depend [no]
+ */
+
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+      (global.LivePPT = factory())
+})(this, function() {
+  'use strict';
+
+  var Version = '1.0.0';
+  
+  function LivePPT() {
+  }
+  
+  var LivePPT$1 = LivePPT;
+  
+  return LivePPT$1;
+})
